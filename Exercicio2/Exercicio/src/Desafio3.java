0037e2979093ab261e94bf547ef792a54675821b
Luizfernando791 <luizfernando.carrupt@gmail.com>
2025-04-23 14:08:47 -0300

    Aprimorando6

---
+++ Exercicio2/Exercicio/src/Desafio3.java
@@ -0,0 +1,33 @@
+package Exercicio2;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Desafio3 {
+    public static void main(String[] args) {
+        System.out.println("||Verificar eleementos duplicados||");
+
+        List<Integer> ListaDeNumeros = new ArrayList<>();
+
+        ListaDeNumeros.add(22);
+        ListaDeNumeros.add(7);
+        ListaDeNumeros.add(45);
+        ListaDeNumeros.add(23);
+        ListaDeNumeros.add(66);
+
+        System.out.println("Lista dos Numeros:" + ListaDeNumeros);
+
+        List<Integer> ListaDeNumerosPares = new ArrayList<>();
+        List<Integer> ListaDeNumerosImpares = new ArrayList<>();
+        
+        for (int numero : ListaDeNumeros) {
+            if (numero % 2 == 0) {
+                ListaDeNumerosPares.add(numero);
+            }else{
+                ListaDeNumerosImpares.add(numero);
+            }
+        }
+
+        System.out.println("Nuemros pares:" + ListaDeNumerosPares);
+        System.out.println("Nuemros Impares:" + ListaDeNumerosImpares);
+        }
+}
